0550d415d83ccd7a8b9ff23d8a2a5cc5e9febb4f
mj <hi.minjungkim@gmail.com>
2020-05-25 15:35:27 +0900

    use typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-docs/react/preset'],
+  webpackFinal: (config) => console.dir(config, { depth: null }) || config,
 };
